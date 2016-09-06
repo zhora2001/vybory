b3c36509f10a913d17d0daf5102447196873e0fc
zhora2001 <zhora2001@gmail.com>
2016-09-07 00:19:49 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,57 @@
+<?php
+
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = ($row);
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    return $data;
+}
+
+$result = csv_to_array(__DIR__.'/diln.csv',';');
+$kus = array();
+//echo print_r($result);
+foreach($result as $var11)
+{
+//echo ($var11[1]); //
+array_push($kus, $var11['kusch']);
+
+}
+$kus = array_unique($kus);
+//echo print_r($kus);
+
+$new_kusch = array();
+$temp_kusch = array();
+
+foreach($kus as $var1)
+{
+$q = '';
+$i = 0;
+foreach ($result as $var11) {
+  if ($var11['kusch'] == $var1)
+   $q .= $var11['n_diln'].", ";
+
+}
+
+echo  $var1;
+echo  $q;
+$temp_kusch[0] = ['kusch'=>$var1, 'n_diln'=>$q];
+//$new_kusch[$i] = ['kusch'=>$var1, 'n_diln'=>$q];
+array_push($new_kusch,$temp_kusch[0]);
+$i += 1;
+}
+echo print_r($new_kusch);
+?>
